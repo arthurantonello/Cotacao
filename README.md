357cfbd6db1384299076d0f92c5c09816e2f0503
Arthur Antonello <94024050+arthurantonello@users.noreply.github.com>
2025-03-19 10:27:38 -0300

    19.03
    Criada a primeira versão do gráfico usando Plotly

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Cotacao
 Realização do desafio proposto pela empresta Abess, da implementação de um sistema de cotação monetária, afim de ingressar na vaga de Dev JR.
+
+Recomendado uso de máquina virtual para execução
+Para utilização do código, favor digitar os seguintes códigos no terminal:
+
+pip install django
+pip install matplotlib 
--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+from plotly.utils import PlotlyJSONEncoder
+import json
 
 def inicio(request):
-    return render(request, 'inicio.html')
+    # xticks = 5
+    # x = [1, 2, 3, 4]
+    # y = [2, 3, 4, 3]
+    # plt.plot(x, y)
+    # plt.title('Cotação monetária')
+    # plt.ylabel('Moedas')
+    # plt.xlabel('Tempo') #Incluir o tamanho da moeda mais cara + 1 para visualização
+    # plt.xticks(range(-(xticks), xticks, 1)) #Função de dias alterará aqui
+    # plt.show()
+
+     # Criando um gráfico interativo
+     # Crie uma figura simples
+    fig = go.Figure()
+    # Adicione dados ao gráfico
+    fig.add_trace(go.Scatter(
+        x=[-5, 1, 2, 3, 4, 5],
+        y=[-5, 10, 15, 7, 12, 22],
+        mode='lines',
+        name='Dados'
+    ))
+
+    grafico_json = json.dumps(fig, cls=PlotlyJSONEncoder)
+    context = {"grafico_json": grafico_json}
+    
+    return render(request, "inicio.html", context)
